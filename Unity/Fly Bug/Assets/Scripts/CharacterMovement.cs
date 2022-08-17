88f3c0deff0e45f8feb978c15894ddaa9ef2123d
carl <carissasunny@gmail.com>
2022-08-17 16:05:25 -0600

    fly bug old return to old input
    and added character controls, plus joystick functionality

--- Unity/Fly Bug/Assets/Scripts/CharacterMovement.cs
+++ Unity/Fly Bug/Assets/Scripts/CharacterMovement.cs
@@ -1,48 +1,32 @@
 using System;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+// [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CharacterController))]
 public class CharacterMovement : MonoBehaviour
 {
-    public CharacterInputSO characterInputsSO;
-    public Joystick joystick;
-    private void OnEnable()
-    {
-        characterInputsSO.characterinputs.Player.TestKey.performed += SpacePressed;
-        characterInputsSO.characterinputs.Player.Move.performed += Move;
-    }
-
-    private void OnDisable()
-    {
-        // characterControls
-    }
-
-    public void SpacePressed(InputAction.CallbackContext obj)
-    {
-        Debug.Log("jump");
-        // rb.velocity = new Vector2(rb.velocity.x, 2);
-    }
-
-    public void Move(InputAction.CallbackContext obj)
-    {
-        Debug.Log(obj);
-    }
-    
-    
-    public void OnMouseDown()
-    {
-        throw new NotImplementedException();
-    }
     private Rigidbody rb;
-    public float speed = 100f;
+    public float speed = 10f;
+    public float turnSpeed = 10f;
     private bool onGround = true;
+    public Joystick joystick;
+    private CharacterController characterController;
+
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        // rb = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
         // onGround = false;
     }
-
+    
+    public void Update()
+    {
+        Vector3 direction = Vector3.up * joystick.Vertical + Vector3.right * joystick.Horizontal;
+        // rb.AddForce(direction * turnSpeed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        if(Input.GetKey(KeyCode.Space)) Launch();
+        characterController.Move(direction * turnSpeed * Time.deltaTime);
+    }
     public void Jump()
     {
         if (onGround)
@@ -50,6 +34,12 @@ public class CharacterMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, speed);
             // print("jump");
         }
+    }
+
+    public void Launch()
+    {
+        // rb.AddForce(Vector3.forward * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        characterController.Move(Vector3.forward * speed * Time.deltaTime);
     }
 
     public void SetOnGround(bool boolVar)
