4322494a297ed6af849d583754e39d78d048399d
carl <carissasunny@gmail.com>
2022-08-22 16:19:28 -0600

    ethics

--- Unity/Fly Bug/Assets/Scripts/CharacterMovement.cs
+++ Unity/Fly Bug/Assets/Scripts/CharacterMovement.cs
@@ -1,50 +1,37 @@
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-// [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(CharacterController))]
 public class CharacterMovement : MonoBehaviour
 {
-    private Rigidbody rb;
     public float speed = 10f;
     public float turnSpeed = 10f;
     private bool onGround = true;
+    private bool flying;
     public Joystick joystick;
     private CharacterController characterController;
 
     private void Awake()
     {
-        // rb = GetComponent<Rigidbody>();
         characterController = GetComponent<CharacterController>();
+        flying = false;
         // onGround = false;
     }
     
     public void Update()
     {
         Vector3 direction = Vector3.up * joystick.Vertical + Vector3.right * joystick.Horizontal;
-        // rb.AddForce(direction * turnSpeed * Time.fixedDeltaTime, ForceMode.VelocityChange);
-        if(Input.GetKey(KeyCode.Space)) Launch();
-        characterController.Move(direction * turnSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.Space)) flying = !flying;
+        if (flying) characterController.Move(Vector3.forward * speed * Time.deltaTime);
+        characterController.Move( direction * turnSpeed * Time.deltaTime);
+        // transform.Rotate(direction * turnSpeed * Time.deltaTime);
     }
-    public void Jump()
-    {
-        if (onGround)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, speed);
-            // print("jump");
-        }
-    }
-
+    
     public void Launch()
     {
-        // rb.AddForce(Vector3.forward * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
-        characterController.Move(Vector3.forward * speed * Time.deltaTime);
-    }
-
-    public void SetOnGround(bool boolVar)
-    {
-        onGround = boolVar;
+        while (flying)
+        {
+            characterController.Move(Vector3.forward * speed * Time.deltaTime);
+        }
     }
 
     public void Death()
