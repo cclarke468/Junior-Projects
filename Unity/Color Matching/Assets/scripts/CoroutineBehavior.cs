52116a3c653a40bfcf5d762cad261695925853bb
Carl <carissasunny@gmail.com>
2022-09-14 06:24:31 -0600

    321go

--- Unity/Color Matching/Assets/scripts/CoroutineBehavior.cs
+++ Unity/Color Matching/Assets/scripts/CoroutineBehavior.cs
@@ -12,30 +12,47 @@ public class CoroutineBehavior : MonoBehaviour
     public float seconds = 3;
     private WaitForFixedUpdate _waitForFixedUpdate;
     public IntData i;
-    public UnityEvent startEvent, repeatEvent, endEvent;
-    private IEnumerator Start()
+    public UnityEvent startEvent, startCountEvent, repeatCountEvent, endCountEvent, repeatUntilFalseEvent;
+    private IEnumerator Counting()
     {
-        // i.value = 3;
         _waitForSeconds = new WaitForSeconds(seconds);
         _waitForFixedUpdate = new WaitForFixedUpdate();
         
-     
-
-        // while (canRun)
-        // {
-        //     yield return _waitForFixedUpdate;
-        //     print("run on start");
-        // }
-        startEvent.Invoke();
+        startCountEvent.Invoke();
         yield return _waitForSeconds;
         // print("late start");
         while (i.value > 0)
         {
-            repeatEvent.Invoke();
+            repeatCountEvent.Invoke();
             i.value--;
             // print(i);
             yield return _waitForSeconds;
         }
-        endEvent.Invoke();
+        endCountEvent.Invoke();
+    }
+
+    private IEnumerator RepeatUntilFalse()
+    {
+        while (canRun)
+        {
+            yield return _waitForFixedUpdate;
+            repeatUntilFalseEvent.Invoke();
+        }
+    }
+
+    public void StartRepeatUntilFalse()
+    {
+        canRun = true;
+        StartCoroutine(RepeatUntilFalse());
+    }
+
+    public void StartCounting()
+    {
+        StartCoroutine(Counting());
+    }
+
+    private void Start()
+    {
+        startEvent.Invoke();
     }
 }
