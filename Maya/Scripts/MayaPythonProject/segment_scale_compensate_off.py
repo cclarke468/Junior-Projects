e43b1ef90fc0c14cea964db2a00d8aa9c54d4604
Carl <carissasunny@gmail.com>
2023-03-31 01:21:03 -0600

    mia global scale also joe

--- Maya/Scripts/MayaPythonProject/segment_scale_compensate_off.py
+++ Maya/Scripts/MayaPythonProject/segment_scale_compensate_off.py
@@ -1,10 +1,9 @@
 import maya.cmds as cmds
 
 def turn_off_segment_scale_compensate():
-    selected_jnts = cmds.ls(selection=True)
+    #selected_jnts = cmds.ls(selection=True)
+    selected_jnts = cmds.ls(type='joint') #selects all joints automatically
     for jnt in selected_jnts:
         if cmds.attributeQuery("segmentScaleCompensate", node=jnt, exists=True):
             cmds.setAttr("%s.segmentScaleCompensate" % jnt, 0, e=True)
-            print jnt
-# setAttr "FK_L_Finger_01_Knuckle_03_Jnt.segmentScaleCompensate" 0;
 turn_off_segment_scale_compensate()
